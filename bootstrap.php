fc7b7812552ff3e1e141f1780cf089080a2510cf
JoshyPHP <s9e.dev@gmail.com>
2017-12-27 13:21:08 +0100

    Updated copyright year

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 /**
 * @package   s9e\mediaembed
-* @copyright Copyright (c) 2015-2016 The s9e Authors
+* @copyright Copyright (c) 2015-2017 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\Flarum\MediaEmbed;
